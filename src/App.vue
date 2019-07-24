cf14f301cb11165581873e1fd4b82c8b83b8add9
4e4e4i <zazevang@gmail.com>
2019-07-24 14:33:54 +0300

    Made Toolbar with drawer on homepage

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,58 @@
 <template>
-  <h1>Hello vue</h1>
+  <v-app>
+    <v-navigation-drawer
+      temporary
+      app
+      v-model="drawer"
+    >
+      <v-list>
+        <v-list-item
+          v-for="(link, index) in links"
+          :key="index"
+          :to="link.url"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="link.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+    </v-navigation-drawer>
+
+    <v-app-bar app color="primary" dark>
+      <v-btn
+        icon
+        @click="drawer = !drawer"
+        class="hidden-md-and-up"
+      >
+        <v-icon>menu</v-icon>
+      </v-btn>
+      <v-toolbar-title>Ad application</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn
+          v-for="(link, index) in links"
+          :key="index"
+          :to="link.url"
+          text
+        >
+          <v-icon left>
+            {{ link.icon }}
+          </v-icon>
+          {{ link.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
@@ -9,7 +62,14 @@ export default {
   components: {
   },
   data: () => ({
-    //
-  }),
-};
+    drawer: false,
+    links: [
+      { title: 'Login', icon: 'lock', url: '/login' },
+      { title: 'Registration', icon: 'face', url: '/registration' },
+      { title: 'Orders', icon: 'bookmark_border', url: '/orders' },
+      { title: 'New ad', icon: 'note_add', url: '/new' },
+      { title: 'My ads', icon: 'list', url: '/list' }
+    ]
+  })
+}
 </script>
