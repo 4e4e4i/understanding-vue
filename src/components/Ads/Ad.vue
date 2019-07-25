0a24b188fc2f0a108e26430754709d160f704343
4e4e4i <zazevang@gmail.com>
2019-07-25 08:47:32 +0300

    Made Ad list and Ad pages

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,24 @@
   <v-container>
     <v-layout row>
       <v-flex xs12>
-        <h1>Ad</h1>
+        <v-card>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/carousel/planet.jpg"
+            height="300"
+          ></v-img>
+          <v-card-text>
+            <h1 class="text--primary mb-3">
+              lorem
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, soluta.
+            </p>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn class="warning">Edit</v-btn>
+            <v-btn class="success">Buy</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
